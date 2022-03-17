51de792df10499e40ec2b17f513ea42993c4ffd6
Bruno <brunofernandes.job@gmail.com>
2022-03-17 16:25:25 -0300

    Add all files from routers and models, like categories, products, orders, users, and made the refactoring from the project

--- backend/app.js
+++ backend/app.js
@@ -2,55 +2,30 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 require("dotenv/config");
-const api = process.env.API_URL;
+
+app.use(cors());
+app.options("*", cors());
 
 //Midleware
 app.use(express.json());
 app.use(morgan("tiny"));
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true,
-    },
-});
+//Routes
+const categoriesRoutes = require("./routers/categories");
+const productsRoutes = require("./routers/products");
+const usersRoutes = require("./routers/users");
+const ordersRoutes = require("./routers/orders");
 
-const Product = mongoose.model("Product", productSchema);
+const api = process.env.API_URL;
 
-app.get(`${api}/products`, async(req, res) => {
-    const productList = await Product.find();
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
-    if (!productList) {
-        res.status(500).json({ sucess: false });
-    }
-
-    res.send(productList);
-});
-
-app.post(`${api}/products`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock,
-    });
-
-    product
-        .save()
-        .then((createdProduct) => {
-            res.status(201).json(createdProduct);
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: err,
-                sucess: false,
-            });
-        });
-});
-
+//Connection with mongodb
 mongoose
     .connect(process.env.CONNECTION_STRING, {
         useNewUrlParser: true,
@@ -64,6 +39,7 @@ mongoose
         console.log(err);
     });
 
+//Server
 app.listen(3000, () => {
     console.log("Servidor rodando em http://localhost:3000");
 });
