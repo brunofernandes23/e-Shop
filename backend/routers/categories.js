cf67787f45738de1e37f49fd2732a370f5509656
Bruno <brunofernandes.job@gmail.com>
2022-03-18 11:20:54 -0300

    add the get method to find category by id

--- backend/routers/categories.js
+++ backend/routers/categories.js
@@ -8,7 +8,18 @@ router.get(`/`, async(req, res) => {
     if (!categoryList) {
         res.status(500).json({ success: false });
     }
-    res.send(categoryList);
+    res.status(200).send(categoryList);
+});
+
+router.get("/:id", async(req, res) => {
+    const category = await Category.findById(req.params.id);
+
+    if (!category) {
+        res.status(500).json({
+            message: "A categoria com o ID fornecido, não foi encontrada",
+        });
+    }
+    res.status(200).send(category);
 });
 
 router.post("/", async(req, res) => {
@@ -29,19 +40,15 @@ router.delete("/:id", async(req, res) => {
     Category.findByIdAndRemove(req.params.id)
         .then((category) => {
             if (category) {
-                return res
-                    .status(200)
-                    .json({
-                        sucess: true,
-                        message: "A categoria foi deletada",
-                    });
+                return res.status(200).json({
+                    sucess: true,
+                    message: "A categoria foi deletada",
+                });
             } else {
-                return res
-                    .status(404)
-                    .json({
-                        sucess: false,
-                        message: "A categoria não foi encontrada!",
-                    });
+                return res.status(404).json({
+                    sucess: false,
+                    message: "A categoria não foi encontrada!",
+                });
             }
         })
         .catch((err) => {
