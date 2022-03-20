9c7319e2c6b605717dda872cd60119bdbbdec99d
Bruno <brunofernandes.job@gmail.com>
2022-03-20 09:09:45 -0300

    Finish the project adding a few things like a new image to test the upload and add the jwt path to acess the uploads files if the user is not authenticated

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -1,19 +1,20 @@
-const expressJwt = require("express-jwt");
+const expressJwt = require('express-jwt');
 
 function authJwt() {
     const secret = process.env.secret;
     const api = process.env.API_URL;
     return expressJwt({
         secret,
-        algorithms: ["HS256"],
-        isRevoked: isRevoked,
+        algorithms: ['HS256'],
+        isRevoked: isRevoked
     }).unless({
         path: [
-            { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
-            { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
             `${api}/users/login`,
-            `${api}/users/register`,
-        ],
+            `${api}/users/register`
+        ]
     });
 }
 
